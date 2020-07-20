4832ca9b29af4169ca67e2ffedb99dc90c784fbd
DCTorres87 <cruz_danielle_torres@yahoo.com>
2020-07-19 23:48:02 -0400

    go through code and DRY up anything that needs it in review controller make sure its restful

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -39,10 +39,6 @@ class ReviewsController < ApplicationController
   
     end 
 
-    def new 
-        @review = Review.new(user_id: params[:user_id]) 
-    end 
-
     def create 
         #retrieves review by attraction id
         #if review dosent' exist create review record useing review paramss
@@ -50,7 +46,6 @@ class ReviewsController < ApplicationController
         #if review saves successfully it will render show page
 
         @review = get_review_by_attraction
-
 
         if @review.nil? 
             @review = current_user.reviews.build(review_params)
@@ -68,10 +63,9 @@ class ReviewsController < ApplicationController
 
     end 
 
-    def edit
-        #retrieves review by review id
-        @review = get_review_by_id
-    end
+    def new 
+        @review = Review.new(user_id: params[:user_id]) 
+    end 
 
     def update
         #retrieves review by review id
@@ -87,6 +81,13 @@ class ReviewsController < ApplicationController
         end
 
     end 
+
+
+    def edit
+        #retrieves review by review id
+        @review = get_review_by_id
+    end
+
 
     def destroy
         @user = current_user
@@ -107,7 +108,6 @@ class ReviewsController < ApplicationController
         @review = Review.find_by(user_id: params[:user_id], id: params[:id]) 
     end
 
-
     def get_review_by_attraction
         #retrieve reviews based on attraction id
         @review = Review.find_by(user_id: params[:user_id], attraction_id: params["review"][:attraction_id])
@@ -118,9 +118,7 @@ class ReviewsController < ApplicationController
         redirect_to user_review_path(@review.user_id,  @review.id)
     end
 
-
-    def review_params
-    
+    def review_params    
         params.require(:review).permit(:stars, :review, :user_id, :attraction_id)
     end   
 
